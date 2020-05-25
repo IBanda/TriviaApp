2bf5944cde13a81ae23aa1fc0b66693dc22955ef
Ian Banda <ian.d.banda@gmail.com>
2020-05-26 04:39:55 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
-import { Home, Login } from './screens/Screens';
+import { Home } from './screens/Screens';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import Search from '../components/Search';
 
 const Home = () => {
   return <Search />;
 };
 
-const styles = StyleSheet.create({});
 export default Home;
